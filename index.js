8fb14394b2ce1df7e758dac4b8995ffcdcf38409
unknown <kirikom94@gmail.com>
2022-08-20 13:57:52 +0530

    First app commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const app = require('./app');
+
+const server = http.createServer(app);
+
+server.listen('5000', () => {
+    console.log('Server is listening now');
+});
+
+// server.on('listening', () => {
+//   console.log('Server is listening now');
+// });
+
