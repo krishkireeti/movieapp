166e97b4a4ad13ff307689623a40ca4ccef00cb6
krishkireeti <kirikom94@gmail.com>
2022-08-20 15:27:06 +0530

    Checking params

--- app.js
+++ app.js
@@ -8,11 +8,16 @@ app.get('/', (req, res, next) => {
     // res.json({
     //     message: "Welcome to the homepage"
     // })
-    res.redirect('/user')
+    // res.redirect('/user')
+    res.send("Welcome to homepage")
 });
 
-app.get('/user', (req, res, next) => {
-    res.send("Welcome to user page after nodemon and improving this");
+app.get('/user/:id/:postId', (req, res, next) => {
+    // console.log(req.query);
+   // console.log(req.params);
+   const host = req.get('Host');
+   console.log(host);
+    res.send("Welcome to user page after nodemon");
 })
 
 module.exports = app;
