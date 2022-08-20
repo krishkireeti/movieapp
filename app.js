2aca81134b1d8586a09d8b6fe54d7b8b15fbe456
unknown <kirikom94@gmail.com>
2022-08-20 14:10:11 +0530

    Adding nodemon and routes

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ const express = require('express');
 
 const app = express();
 
+// routes
+app.get('/', (req, res, next) => {
+    // res.status(200).send('Welcome to the homepage');
+    // res.json({
+    //     message: "Welcome to the homepage"
+    // })
+    res.redirect('/user')
+});
+
+app.get('/user', (req, res, next) => {
+    res.send("Welcome to user page after nodemon")
+})
+
 module.exports = app;
